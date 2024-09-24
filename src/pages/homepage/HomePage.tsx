de5f881fd3410d27d2a5e90ac00a41ad197c0c90
Liam Xu Martello <lxu@martellotech.com>
2024-09-24 16:05:09 -0400

    chart display available

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -5,8 +5,15 @@ import {
   MessageList,
   Message,
   MessageInput,
+  MessageCustomContent,
+  MessageHtmlContent,
+  TypingIndicator,
 } from '@chatscope/chat-ui-kit-react';
 import React, { useState } from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar, Chart } from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 enum Sender {
   Left = 'left',
@@ -18,46 +25,98 @@ type ChatMessage = {
 };
 
 const Homepage = () => {
-  const [query, setQuery] = useState('');
+  const [ifBotTyping, setIfBotTyping] = useState(false);
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([
     {
       message: 'Hello, Please enter your question!',
       sender: Sender.Left,
     },
   ]);
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const aiResponse = (await fetchOpenAIResponse(query)).data;
-    console.log(aiResponse);
-    // setResponse(aiResponse);
-  };
 
   // Function to handle user messages
   const handleUserMessage = async (userMessage) => {
     // Create a new user message object
     const newUserMessage = {
       message: userMessage,
-      sender:  Sender.Right,
+      sender: Sender.Right,
+    };
+    let updatedChatMessages = [...chatMessages, newUserMessage];
+    setChatMessages(updatedChatMessages);
+
+    setIfBotTyping(true);
+    const aiResponse = (await fetchOpenAIResponse(userMessage)).data;
+    console.log(aiResponse);
+
+    const newBotMessage = {
+      message: aiResponse,
+      sender: Sender.Left,
     };
 
-    // Update chat messages state with the new user message
-    const updatedChatMessages = [...chatMessages, newUserMessage];
+    updatedChatMessages = [...chatMessages, newBotMessage];
     setChatMessages(updatedChatMessages);
   };
 
+  const labels = ['jan', 'feb', 'mar', 3, 4, 5, 6, 7];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <>
       {/* A container for the chat window */}
-      <div style={{ position: 'relative', height: '100vh', width: '700px', backgroundColor:'black' }}>
+      <div
+        style={{
+          position: 'relative',
+          height: '80vh',
+          width: '700px',
+          backgroundColor: 'black',
+        }}
+      >
         {/* All components are wrapped in the MainContainer */}
         <MainContainer>
           {/* All chat logic will be contained in the ChatContainer */}
           <ChatContainer>
             {/* Shows all our messages */}
             <MessageList>
+              {/* <h3 style={{ textAlign: 'center', marginBottom: '10px' }}>
+                Chat Messages
+              </h3> */}
+              {/* <Bar data={data} />; */}
+              <Message
+                model={{
+                  message: '',
+                  direction: 'outgoing',
+                  position: 'single',
+                }}
+              >
+                <Message.CustomContent>
+                  <Bar data={data} />
+                </Message.CustomContent>
+              </Message>
               {chatMessages.map((message, i) => {
                 return (
                   <Message
@@ -82,6 +141,7 @@ const Homepage = () => {
           </ChatContainer>
         </MainContainer>
       </div>
+      {ifBotTyping ? <TypingIndicator content="VDX Bot is typing" /> : <></>}
       <MessageInput
         placeholder="Type Message here"
         onSend={handleUserMessage}
